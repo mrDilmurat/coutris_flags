57e1040073187d42fdc735b119e1fb54470298da
Dilmurat Idrisov <dilmuratidrisov@MacBook-Air-Dilmurat.local>
2023-01-20 15:10:21 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Классная работа. Асинхронность, промисы и HTTP.
+
+// Используя API списка всех стран
+// выведите таблицу с информацией о
+// всех странах наподобие такой:
+// API: https://restcountries.com/v3.1/all
+
+let res = fetch("https:restcountries.com/v3.1/all");
+
+res
+  .then((result) => result.json())
+  .then((data) => {
+    tbody = document.querySelector("tbody");
+    console.log(data);
+    data.forEach((item) => {
+      tbody.innerHTML += `
+        <tr>
+            <td>${item.cca2}</td>
+            <td><img class='img' src="${item.flags.svg}"></td>
+            <td>${item.name.official}</td>
+            <td>${item.capital}</td>
+            <td>${item.population}</td>
+        </tr>
+        `;
+      // $('tbody').append(elem)
+      // $('.img').css('background-size', 'contain')
+    });
+  });
+
+console.log("sgvsfg");
